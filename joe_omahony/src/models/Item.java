6b3258b531764e1db6838cb224f63dc78f707c7f
JoeOMahony <21788075@mail.wit.ie>
2024-12-17 13:08:47 +0000

    Item class complete
    The test can't run until the arraylists are set up to hold items, will come back later

--- joe_omahony/src/models/Item.java
+++ joe_omahony/src/models/Item.java
@@ -1,5 +1,7 @@
 package models;
 
+import static utils.Utilities.*;
+
 public class Item {
 /*The responsibility for this class is to manage a single Item in the
 system. An item belongs to a specific Note.
@@ -9,12 +11,12 @@ GENERATE getters, setters, constructors and toString methods. They are only
  basic methods and may need to be modified e.g. to add validation, change
  the format of the generated toString, etc.*/
     public Item(String itemDescription) {
-        this.itemDescription = itemDescription;
+        this.setItemDescription(itemDescription);
     }
 
     public Item(String itemDescription, boolean isItemCompleted) {
-        this.itemDescription = itemDescription;
-        this.isItemCompleted = isItemCompleted;
+        this.setItemDescription(itemDescription);
+        this.setItemCompleted(isItemCompleted); // two param constructor used
     }
 
 /*
@@ -23,27 +25,37 @@ listed above.
 */
 
     public String getItemDescription() {
-        return itemDescription;
+        return this.itemDescription;
     }
 
     public void setItemDescription(String itemDescription) {
-        this.itemDescription = itemDescription;
+        if (validateStringLength(itemDescription, 50)) {
+            this.itemDescription = itemDescription;
+        }
+        else {
+            this.itemDescription = truncateString(itemDescription, 50);
+        }
     }
 
     public boolean isItemCompleted() {
-        return isItemCompleted;
+        return this.isItemCompleted;
     }
 
     public void setItemCompleted(boolean itemCompleted) {
-        isItemCompleted = itemCompleted;
+        this.isItemCompleted = itemCompleted;
     }
 
+    @Override // must override as by default only checks memory position
     public boolean equals(Object obj) {
+        if (obj instanceof Item) {
+            Item item = (Item) obj; // still object, must cast to access fields
+            return (this.itemDescription.equals(item.getItemDescription()) && this.isItemCompleted == item.isItemCompleted);
+        }
         return false;
     }
 
 /* Sample output from the toString is below (note how the boolean value
-for isItemcompleted is set to either TODO or Completed when printing to
+for isItemCompleted is set to either TODO or Completed when printing to
 the console):
 Secondary School 3:30. [Completed]
 Primary School 2.30. [TODO]
@@ -53,11 +65,19 @@ Book hotel. [COMPLETED]
 
     @Override
     public String toString() {
-        return "Item{" + "itemDescription='" + itemDescription + '\'' + ", isItemCompleted=" + isItemCompleted + '}';
+        String itemFormat = "";
+        itemFormat += this.getItemDescription() + "\t";
+        if (this.isItemCompleted()) {
+            itemFormat += "[COMPLETED]";
+        }
+        else {
+            itemFormat += "[TODO]";
+        }
+        return itemFormat;
     }
 /*
 itemDescription: The description is maximum 50 characters. When creating a
-new item, if the string is longer than 50 characters, you shoud truncate the
+new item, if the string is longer than 50 characters, you should truncate the
  string to the first 50 chars (Hint see Utilities for a useful method to do
   this) When updating itemDescription, you should only update if the value
   is less than or equal to 50.
